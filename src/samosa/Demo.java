cf4bb9ee56c7cda20138e936d3f58fadbe2da55a
Kshitij <kshitij_betharia@rediffmail.com>
2025-02-16 17:02:02 +0530

    my project

---
+++ src/samosa/Demo.java
@@ -0,0 +1,19 @@
+package samosa;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		  int a = 10, b = 20;
+
+	        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+	        // Swapping using a temporary variable
+	        int temp = a;
+	        a = b;
+	        b = temp;
+
+	        System.out.println("After swapping: a = " + a + ", b = " + b);
+
+	}
+
+}
